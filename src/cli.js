ac7083e82a6a799330784ecd4495b6cde8536126
Iuliia Davidenko <idavidenko.ext@diamos.com>
2021-11-27 14:36:16 +0300

    third

---
+++ src/cli.js
@@ -0,0 +1,25 @@
+//import readlineSync from 'readline-sync';
+//import readlineSync from './readline-sync';
+
+import readlineSync from "readline-sync";
+
+//const name = readlineSync.question('Your answer: ');
+
+export const greeting = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    //return 0;
+}
+
+//export { cube };
+
+
+/*
+*
+* $ brain-games
+Welcome to the Brain Games!
+May I have your name? John
+Hello, John!
+*
+* */
