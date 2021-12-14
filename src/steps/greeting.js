d0b90105b18fd1950464a691dbc522d6c05610a6
Iuliia Davidenko <idavidenko.ext@diamos.com>
2021-12-14 13:12:01 +0300

    notice1

--- src/steps/greeting.js
+++ src/steps/greeting.js
@@ -1,4 +1,4 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 const greeting = (greetingText, introQuestion, whatShouldYouDo) => {
   console.log(greetingText);
--- src/steps/guessAnswers.js
+++ src/steps/guessAnswers.js
@@ -1,4 +1,4 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 const guessAnswers = (expression, name, realAnswer) => {
   console.log(`Question: ${expression}`);
