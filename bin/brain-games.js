9e985195c84deb0d4310dded5fb309473e0c996a
idavidenko.ext@diamos.com <idavidenko.ext@diamos.com>
2021-12-22 13:49:06 +0300

    notice 1

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,15 +1,4 @@
 #!/usr/bin/env node
-
-import readlineSync from 'readline-sync';
-import { greetingText, introQuestion } from '../src/consts.js';
-import brainGames from "../src/games/brain-games-all.js";
-
-/*const start = () => {
-  console.log(greetingText);
-  const name = readlineSync.question(introQuestion);
-  console.log(`Hello, ${name}!`);
-};*/
-
-//start();
+import brainGames from '../src/games/brain-games-all.js';
 
 brainGames();
--- src/games/brain-games-all.js
+++ src/games/brain-games-all.js
@@ -1,15 +1,9 @@
-import {greetingText, introQuestion} from "../consts.js";
-import readlineSync from "readline-sync";
-import greeting from "../steps/greeting.js";
+import { greetingText, introQuestion } from '../consts.js';
+import greeting from '../steps/greeting.js';
 
 
 const brainGames = () => {
   greeting(greetingText, introQuestion);
-  /*startGame(brainEvenLogics, name);
-
-  console.log(greetingText);
-  const name = readlineSync.question(introQuestion);
-  console.log(`Hello, ${name}!`);*/
 };
 
 export default brainGames;
