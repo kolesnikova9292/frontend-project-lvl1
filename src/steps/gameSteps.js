c44be7a1d096075252b66da56e7d7add0ac9cd72
idavidenko.ext@diamos.com <idavidenko.ext@diamos.com>
2022-01-26 20:58:10 +0300

    notices

--- src/steps/gameSteps.js
+++ src/steps/gameSteps.js
@@ -1,24 +1,5 @@
 import readlineSync from 'readline-sync';
 
-const startGame = (greetingText, introQuestion, logicOfGame = null, whatShouldYouDo = '', stepsCount = 3) => {
-  console.log(greetingText);
-  const name = readlineSync.question(introQuestion);
-  console.log(`Hello, ${name}!`);
-  if (whatShouldYouDo) console.log(whatShouldYouDo);
-
-  if(logicOfGame) {
-    for (let step = 0; step < stepsCount; step += 1) {
-      const { expression, rightAnswer } = logicOfGame();
-
-      const result = guessAnswers(expression, name, rightAnswer);
-
-      if (result === 'fail') {
-        return;
-      }
-    }
-  }
-}
-
 const guessAnswers = (expression, name, realAnswer) => {
   console.log(`Question: ${expression}`);
 
@@ -34,5 +15,24 @@ const guessAnswers = (expression, name, realAnswer) => {
   console.log(`Congratulations, ${name}!`);
   return true;
 };
+
+const startGame = (greetingText, introQuestion, logicOfGame = null, whatShouldYouDo = '', stepsCount = 3) => {
+  console.log(greetingText);
+  const name = readlineSync.question(introQuestion);
+  console.log(`Hello, ${name}!`);
+  if (whatShouldYouDo) console.log(whatShouldYouDo);
+
+  if (logicOfGame) {
+    for (let step = 0; step < stepsCount; step += 1) {
+      const { expression, rightAnswer } = logicOfGame();
+
+      const result = guessAnswers(expression, name, rightAnswer);
+
+      if (result === 'fail') {
+        return;
+      }
+    }
+  }
+}
 
 export { guessAnswers, startGame };
